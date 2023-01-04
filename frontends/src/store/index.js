7e536b492059fc8d07abf6892eca3fdda78cf1b5
Nestor Vega <vega.nestor1@gmail.com>
2023-01-04 15:14:13 -0800

    frontend

---
+++ frontends/src/store/index.js
@@ -0,0 +1,27 @@
+import { createStore } from "redux";
+import { combineReducers } from "redux";
+import { applyMiddleware } from "redux";
+import { compose } from "redux";
+import thunk from "redux-thunk";
+import session from './session'
+
+let enhancer;
+export const rootReducer = combineReducers({
+    session
+})
+
+if (process.env.NODE_ENV === 'production') {
+    enhancer = applyMiddleware(thunk);
+} else {
+    const logger = require('redux-logger').default;
+    const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+
+const configureStore = (preloadState = {}) => {
+    return createStore(rootReducer, preloadState, enhancer)
+}
+
+export default configureStore
