4fe204b076669c3ad314e81527aafd778ff3747e
Nestor Vega <vega.nestor1@gmail.com>
2023-01-29 22:16:50 -0800

    links on profile page

--- frontend/src/components/profilePage/index.js
+++ frontend/src/components/profilePage/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from "react-redux";
-import { Redirect } from 'react-router-dom';
+import { NavLink, Redirect } from 'react-router-dom';
 import PostIndex from '../PostIndexItem';
 import './profilePage.css'
 
@@ -10,7 +10,7 @@ const ProfilePage = () => {
   if (user){
     return (
       <>
-          <div className='bio'>
+          <div className='header'>
             <div className='profilePicture'>
               <img src="images/bpfp.png"/>
               <div className='profileName'>
@@ -18,6 +18,16 @@ const ProfilePage = () => {
               </div>
             </div>
           <hr/>
+          </div>
+          <div className='links'>
+              <h4 class="text-uppercase mb-4">Around the Web</h4>
+              <a href="https://twitter.com/nvega24">twitter<i class="fab fa-fw fa-twitter"></i></a>
+              <br/>
+              <a href="https://angel.co/u/nestorvega23">wellfound<i class="fab fa-angellist"></i></a>
+              <br/>
+              <a href="https://www.linkedin.com/in/nestor-vega-233b43238/">linkedin<i class="fab fa-fw fa-linkedin-in"></i></a>
+              <br/>
+              <a href="https://github.com/nvega23">Github<i class="fab fa-fw fa-github"></i></a>
           </div>
           <div className='headers'>
             <PostIndex/>
