93fed3572f6903a16ba3af352a69f560cf32a0ec
Nestor Vega <vega.nestor1@gmail.com>
2023-01-30 16:33:19 -0800

    fixed associations

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -12,12 +12,12 @@ class Friend < ApplicationRecord
     validates :requester_id, presence: true, uniqueness: { scope: :requestee_id }
     validates :requestee_id, presence: true
 
+    belongs_to :requestee,
+    foreign_key: :requestee_id,
+    class_name: :User
+
     belongs_to :user,
         foreign_key: :requester_id,
-        class_name: :User
-
-    belongs_to :requestee_id,
-        foreign_key: :requestee_id,
         class_name: :User
 
 end
