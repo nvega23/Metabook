053b2acd49d07d02c9535784b39ca398a9c76868
Nestor Vega <vega.nestor1@gmail.com>
2023-02-10 17:33:00 -0800

    edit comment opens another textarea

--- frontend/src/components/postindex/comment.js
+++ frontend/src/components/postindex/comment.js
@@ -17,6 +17,7 @@ const CommentButton = ({post}) => {
 
     const [commentBody, setCommentBody] = useState("");
     const [commentBool, setCommentBool] = useState(false)
+    const [editBool, setEditBool] = useState(false)
 
     const handleCommentPost = async e => {
       e.preventDefault()
@@ -47,7 +48,7 @@ const CommentButton = ({post}) => {
         {comments?.map(comment => (
             <>
                 <form onSubmit={handleDeleteComment}>
-                    <button className="editComment" onClick={(e)=>handleEditComment(e, comment.id)}>
+                    <button className="editComment" onClick={(e)=>handleEditComment(e, comment.id, setEditBool(prev => !prev))}>
                         Edit comment
                     </button>
                     <br/>
@@ -70,8 +71,52 @@ const CommentButton = ({post}) => {
                 Write a comment...
             </button>
             </form>}
+            { editBool && <form>
+                <textarea className="commentText" value={commentBody} onChange={e => setCommentBody(e.target.value)} />
+                <br/>
+                <br/>
+                {comments.map(comment=> (
+                    <button className="writeCommentButton" onClick={(e)=>handleEditComment(e, comment.id, setEditBool(prev => !prev))}>
+                        update comment...
+                    </button>
+                ))}
+            </form>}
         </>
     )
 };
 
 export default CommentButton;
+
+// <>
+// <form onSubmit={handleDeleteComment}>
+//     <button className="editComment" onClick={(e)=>handleEditComment(e, comment.id, setEditBool(prev => !prev))}>
+//         Edit comment
+//     </button>
+//     <br/>
+//     <button className="removeComment" onClick={(e)=>handleDeleteComment(e, comment.id, comment.body)}>
+//     <img src="../images/trashpic.png" alt="trash icon"/> comment
+//     </button>
+// </form>
+//     <>
+//         <p className="commentBody">
+//             {comment?.user?.username.charAt(0).toUpperCase() + comment?.user?.username.slice(1)}: {comment.body}
+//         </p>
+//     </>
+// </>
+// ))}
+// { commentBool && <form>
+// <textarea className="commentText" value={commentBody} onChange={e => setCommentBody(e.target.value)} />
+// <br/>
+// <br/>
+// <button className="writeCommentButton" onClick={handleCommentPost}>
+// Write a comment...
+// </button>
+// </form>}
+// {  editBool && <form>
+// <textarea className="commentText" value={commentBody} onChange={e => setCommentBody(e.target.value)} />
+// <br/>
+// <br/>
+// <button className="writeCommentButton" onClick={handleEditComment}>
+// edit comment...
+// </button>
+// </form>}
