7c0268f8f3670293f225e3d805426b607bc45c7b
Nestor Vega <vega.nestor1@gmail.com>
2023-02-17 22:55:16 -0800

    contact me links

---
+++ frontend/src/components/postindex/links.jsx
@@ -0,0 +1,21 @@
+import "./links.css"
+const PersonalLinks = () => {
+    const openExternalSite = site => {
+        window.open(site);
+    };
+
+    return (
+        <>
+            <div className="diva">
+                <h3 className="contactText">Contact Me</h3>
+                    <a className="contactButtons" target="_blank" href="https://github.com/nvega23">Github</a>
+                    <a className="contactButtons" target="_blank" href="https://twitter.com/nvega24">Twitter</a>
+                    <a className="contactButtons" target="_blank" href="https://www.linkedin.com/in/nestor-vega-233b43238/">Linkedin</a>
+                    <a className="contactButtons" target="_blank" href="https://angel.co/u/nestorvega23">Wellfound</a>
+                    <button className="contactMe" onClick={() => openExternalSite("mailto:vega.nestor1@gmail.com")}><a className="emailMe">Email The Developer</a></button>
+            </div>
+        </>
+    )
+};
+
+export default PersonalLinks;
