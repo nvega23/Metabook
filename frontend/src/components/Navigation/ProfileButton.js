f838c749785668b91f56059f55dadf8be094d60e
Nestor Vega <vega.nestor1@gmail.com>
2023-04-14 11:34:04 -0700

    dropdown menu works

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -56,10 +56,10 @@ function ProfileButton({ user }) {
                   {user.username.charAt(0).toUpperCase() + user?.username.slice(1)}
                 </button>
               </>
-              <button className="dropdown"><a className="twitterLink" target="_blank" href="https://github.com/nvega23">Github</a></button>
-              <button className="dropdown"><a className="twitterLink" target="_blank" href="https://www.linkedin.com/in/nestor-vega-233b43238/">Linkedin</a> </button>
-              <button className="dropdown"><a className="twitterLink" target="_blank" href="https://angel.co/u/nestorvega23">Wellfound</a></button>
-              <button className="dropdown"><a className="twitterLink" target="_blank" href="https://twitter.com/nvega24">Twitter</a></button>
+              <a className="dropdownNoButton" target="_blank" href="https://github.com/nvega23">Github</a>
+              <a className="dropdownNoButton" target="_blank" href="https://www.linkedin.com/in/nestor-vega-233b43238/">Linkedin</a>
+              <a className="dropdownNoButton" target="_blank" href="https://angel.co/u/nestorvega23">Wellfound</a>
+              <a className="dropdownNoButton" target="_blank" href="https://twitter.com/nvega24">Twitter</a>
               <button className="dropdown" onClick={() => openExternalSite(window.open('mailto:vega.nestor1@gmail.com?subject=subject&body=body'))}><a className="twitterLink">Email The Developer</a></button>
               <button className="dropdown" onClick={logout}>Log Out</button>
           </div>
